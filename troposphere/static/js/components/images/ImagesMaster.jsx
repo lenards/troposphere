a30577e39703883e5f4d5500a19b4342fc20ab59
Andrew J Lenards <andrew.lenards@gmail.com>
2017-02-24 20:49:51 +0000

    Add meta tags to root view of Image Catalog

--- troposphere/static/js/components/images/ImagesMaster.jsx
+++ troposphere/static/js/components/images/ImagesMaster.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import Router from "react-router";
+import DocumentMeta from 'react-document-meta';
+
 import SecondaryImageNavigation from "./common/SecondaryImageNavigation";
 
+import globals from "globals";
+
+
 let RouteHandler = Router.RouteHandler;
+
+
+const includeDocumentMeta = () => {
+    let logoImage = `${window.location.origin}${globals.THEME_URL}/images/large_logo.png`,
+        meta = {
+            meta: {
+                title: `${globals.SITE_FOOTER} - ${globals.SITE_TITLE}`,
+                description: 'A cloud for data-driven scientific discovery...',
+                name: {
+                    keywords: ''
+                },
+                property: {
+                    'og:title': `${globals.SITE_FOOTER} - ${globals.SITE_TITLE}`,
+                    'og:image': logoImage,
+                    'og:url': window.location.href,
+                    'og:description': 'A cloud for data-driven scientific discovery...',
+                    'og:site_name': globals.SITE_TITLE,
+                }
+            }
+        };
+
+    return (
+        <DocumentMeta {...meta} />
+    );
+}
+
 
 export default React.createClass({
     displayName: "ImagesMaster",
@@ -10,6 +41,7 @@ export default React.createClass({
     render: function() {
         return (
         <div>
+            { includeDocumentMeta() }
             <SecondaryImageNavigation currentRoute="todo-remove-this" />
             <RouteHandler/>
         </div>
